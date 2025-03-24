e82f28663f7c4d502fc463f8842a7449377c9e2e
Isai-pexel <ibadillam@vlacademy.com>
2025-03-24 10:06:23 -0600

    Editar

--- to-do-list/src/components/ToDoList.vue
+++ to-do-list/src/components/ToDoList.vue
@@ -1,15 +1,40 @@
 <template>
   <h1>{{ titulo }}</h1>
   <div class="formulario">
-    <input />
-    <button>Add</button>
+    <input v-model="tareaNueva" />
+    <button v-on:click="agregarElemento">Add</button>
   </div>
 
-  <h2>Task List</h2>
-
-  <ul>
-    <li>wash dishes</li>
+  <h2>Tareas Pendientes</h2>
+  <p v-if="tareasPendientes === 0">
+    <span>No hay tareas pendientes</span>
+  </p>
+  <ul v-else>
+    <li v-for="(tarea, index) in listaDeTareas" v-bind:key="index">
+      {{ index }}-{{ tarea }}
+      <button v-on:click="eliminarElemento(index)">Completar</button>
+    </li>
   </ul>
+  <!-- <pre>
+    {{ listaDeTareas }}
+  </pre> -->
+
+  <h2>Tareas Completadas</h2>
+  <p v-if="ContadorDeTareasCompletadas === 0">
+    <span>No hay tareas completadas</span>
+  </p>
+  <ul v-else>
+    <li v-for="(listaEliminados, index) in tareasCompletadas" v-bind:key="index">
+      {{ listaEliminados }}
+    </li>
+  </ul>
+
+  <h2>
+    Resumen de tareas
+  </h2>
+  <!-- length muestra el tamaño de la lista -->
+  <p>Completadas: {{ ContadorDeTareasCompletadas }} | Pendientes: {{ tareasPendientes }}</p>
+  <p>Total de tareas: {{ totalDeTareas }}</p>
 </template>
 
 <script>
@@ -17,11 +42,43 @@ export default {
   data() {
     return {
       titulo: 'Aprendizaje de Vue',
-      listaDeTareas: ['Instalar Vue'],
+      listaDeTareas: ['Instalar Vue', 'Abrirlo en el navegador'],
+      tareaNueva: '',
+      tareasCompletadas: [],
     }
+  },
+
+  computed:{
+    tareasPendientes(){
+      return this.listaDeTareas.length
+
+    },
+
+    ContadorDeTareasCompletadas(){
+      return this.tareasCompletadas.length
+    },
+
+    totalDeTareas(){
+      return this.tareasPendientes + this.ContadorDeTareasCompletadas
+    }
+
+  },
+
+  methods: {
+    agregarElemento() {
+      this.listaDeTareas.push(this.tareaNueva)
+    },
+
+    eliminarElemento(index) {
+      //   alert('Eliminar elemento en la posición #' + index)
+      this.tareasCompletadas.push(this.listaDeTareas[index])
+      this.listaDeTareas.splice(index, 1)
+    },
+
   },
 }
 </script>
+import EditorDeToDoList from './EditorDeToDoList.vue'
 
 <style scoped>
 h3 {
